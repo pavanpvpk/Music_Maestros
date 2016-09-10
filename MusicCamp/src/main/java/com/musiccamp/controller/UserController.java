dcb3f2e1a5702a755f5bac7ae9b60f0ff4457a9f
PushkarKanikicherla <pushkerkumar2@gmail.com>
2016-09-10 16:14:30 -0500

    No Changes

--- MusicCamp/src/main/java/com/musiccamp/controller/UserController.java
+++ MusicCamp/src/main/java/com/musiccamp/controller/UserController.java
@@ -12,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
 import com.musiccamp.repositories.UserRepository;
-
-
 import com.musiccamp.entities.UserLogin;
 
 /**
