c9064d28e82e24c34aa8e96abf36ef83a1df6b6c
Manasa <manasarao.283@gmail.com>
2016-10-20 22:19:41 -0500

    Sorting feature implemented in ViewStudentDetails page to sort column names.

---
+++ MusicCamp/src/main/resources/static/assets/js/sort.js
@@ -0,0 +1,16 @@
+
+/*
+Sorting table by headers--Manasa Bojja
+*/
+$(document).ready(function() {
+	
+	if($('body').is('.PageType')){
+		alert("Hello World");
+
+	    $("#myTable").tablesorter();
+
+	}
+	
+}); 
+
+
