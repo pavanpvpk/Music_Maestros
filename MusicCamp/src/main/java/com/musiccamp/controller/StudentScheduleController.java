6814c140ca8d351fda503e6384f14b9d56862d8c
Swathipearl19 <RP046731@cerner.net>
2016-11-06 23:39:36 -0600

    Student Schedule Backend

--- MusicCamp/src/main/java/com/musiccamp/controller/StudentScheduleController.java
+++ MusicCamp/src/main/java/com/musiccamp/controller/StudentScheduleController.java
@@ -1,23 +1,138 @@
 package com.musiccamp.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.musiccamp.entities.ElectiveRoomTimings;
+import com.musiccamp.entities.Electives;
+import com.musiccamp.entities.Room;
+import com.musiccamp.entities.RoomTimings;
+import com.musiccamp.entities.Student;
+import com.musiccamp.entities.Timings;
+import com.musiccamp.model.StudentScheduleModel;
+import com.musiccamp.repositories.ERTRepository;
+import com.musiccamp.repositories.ElectiveRepository;
+import com.musiccamp.repositories.RTRepository;
+import com.musiccamp.repositories.RoomRepository;
+import com.musiccamp.repositories.StudentRepository;
+import com.musiccamp.repositories.TimingRepository;
+
 
 @Controller
 @Scope(value="session")
 public class StudentScheduleController {
+	
+	@Autowired
+	private StudentRepository studentRepo;
 
+	@Autowired
+	private ElectiveRepository electivesRepo;
 	
-	@RequestMapping("/schedulegen")
+	@Autowired
+	private ERTRepository ertRepository;
 	
-	public String Schedulegen(ModelMap model,@RequestParam("studentName") String studentName){
+	
+	@Autowired
+	private RTRepository rtRepository;
+	
+	@Autowired
+	private RoomRepository roomRepository;
+	
+	@Autowired
+	private TimingRepository timingRepository;
+	
+	
+	@RequestMapping(value="/studentSchedule",method=RequestMethod.GET)
+	
+	public String Schedulegen(ModelMap model,HttpSession session){
+		
+		System.out.println(session.getAttribute("username"));
+		//System.out.println(studentName);
+		Integer username =null;
+		if(session != null) {
+		 username = (Integer)session.getAttribute("username");
+		}
+		
+		Student student = studentRepo.find(524980); 
 		
 		
-		System.out.println(studentName);
+		List<String> electiveNames = java.util.Collections.checkedList(new ArrayList<String>(), String.class);
 		
-		return "gensuccess";
+		if(student.getElective1()!=null&&(!student.getElective1().isEmpty())) electiveNames.add(student.getElective1());
+		
+		if(student.getElective2()!=null&&(!student.getElective2().isEmpty())) electiveNames.add(student.getElective2());
+		
+		if(student.getElective3()!=null&&(!student.getElective3().isEmpty())) electiveNames.add(student.getElective3());
+		
+		if(student.getElective4()!=null&&(!student.getElective4().isEmpty())) electiveNames.add(student.getElective4());
+		
+		List<Electives> allElectives = electivesRepo.findAll();
+		List<Electives> studentElectives = new ArrayList<Electives>(); 
+		for (Electives elective	 : allElectives) {
+			if(electiveNames.contains(elective.getElectiveName())) {
+				studentElectives.add(elective);
+			}
+		}
+		
+		System.out.println(studentElectives.size());
+		List<ElectiveRoomTimings> allERTimings = ertRepository.findAll();
+		List<ElectiveRoomTimings> studentERTimings = new ArrayList<ElectiveRoomTimings>(); 
+		for (ElectiveRoomTimings erTimings : allERTimings) {
+			if(studentElectives.contains(erTimings.getElectives()) ) {
+				studentERTimings.add(erTimings);
+			}
+			
+		}
+		
+		List<RoomTimings> allRoomTimings = rtRepository.findAll();
+		List<RoomTimings> studentRTimings = new ArrayList<RoomTimings>(); 
+		for (RoomTimings rTimings : allRoomTimings) {
+			for(ElectiveRoomTimings studentERTiming : studentERTimings ) {
+				if(studentERTiming.getRoomTimings().getRtId() == rTimings.getRtId()) {
+					studentRTimings.add(rTimings);
+				}
+			}
+			
+		}
+		
+		List<Room> allRooms = roomRepository.findAll();
+		List<Timings> allTimings = timingRepository.findAll();
+		List<Room> studentRooms = new ArrayList<Room>(); 
+		List<Timings> studentTimings = new ArrayList<Timings>(); 
+		for(RoomTimings rt:studentRTimings) {
+			for(Room r:allRooms ) {
+				if(r.getRoomId() == rt.getRoom().getRoomId()) {
+					studentRooms.add(r);
+				}
+			}
+			for(Timings t:allTimings ) {
+				if(t.getTimeId() == rt.getTimings().getTimeId()) {
+					studentTimings.add(t);
+				}
+			}
+		}
+		List<StudentScheduleModel> studentScheduleList = new ArrayList<StudentScheduleModel>();
+		for(int i=0;i<studentElectives.size();i++) {
+			StudentScheduleModel ssm =new StudentScheduleModel();
+			ssm.setCourseName(studentElectives.get(i).getElectiveName());
+			ssm.setRoomName(studentRooms.get(i).getRoomName());
+			ssm.setTimings(studentTimings.get(i).getTimeSlot());
+			studentScheduleList.add(ssm);
+			}
+		
+		session.setAttribute("scheduleList", studentScheduleList);
+		return "studentSchedule";
 	}
 }
