ef77b095cac2a45882f6c8384f28adb03c117b1e
S525250 <S525250@NW32221.nwmissouri.edu>
2016-11-20 00:34:36 -0600

    Adding New Functionality to add New Electives in Future!!

---
+++ MusicCamp/src/main/java/com/musiccamp/controller/NewElectiveController.java
@@ -0,0 +1,43 @@
+package com.musiccamp.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.musiccamp.entities.Electives;
+import com.musiccamp.repositories.ElectiveRepository;
+
+
+
+@Controller
+public class NewElectiveController {
+	
+	@Autowired
+	private ElectiveRepository ers;
+	
+	
+	@RequestMapping(value="/addElectives",method=RequestMethod.POST)
+	public String AddNewElective(@RequestParam("newElective") String newElective, ModelMap model){
+		
+		
+	int newelect=ers.InsertnewElective(newElective);
+			if(!(newelect==0)){
+				
+				model.put("success", "New Elective Added");
+				return "redirect:/addElectives";
+				
+			}
+			else{
+				
+				model.put("failure", "something went wrong");
+				return "redirect:/addElectives";
+			}
+		
+	}
+
+}
--- MusicCamp/src/main/java/com/musiccamp/repositories/ElectiveRepository.java
+++ MusicCamp/src/main/java/com/musiccamp/repositories/ElectiveRepository.java
@@ -2,7 +2,9 @@ package com.musiccamp.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.musiccamp.entities.Electives;
 import java.lang.Integer;
@@ -20,4 +22,9 @@ public interface ElectiveRepository extends JpaRepository<Electives, Integer> {
 	@Query(value="select e.electiveId from Electives e where e.electiveName=?1")
 	public int findbyelectiveID(String electivenName);
 
+	@Transactional
+	@Modifying
+	@Query(value="insert into electives(elective_name) values (?1)",nativeQuery=true)
+	public int InsertnewElective(String newelectiveName);
+	
 }
